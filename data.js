ea0f90c4241b876281f3d0e2b03915a4c59ef4d1
Annette Donald Blackburn <109121304+annette-blackburn@users.noreply.github.com>
2022-10-09 16:44:31 -0600

    Add files via upload

--- data.js
+++ data.js
@@ -755,3 +755,247 @@ var data = [
         durationMinutes: "2 seconds",
         comments: "3 Fast-moving lights over Round Rock Texas"
     },
+    {
+        datetime: "1/10/2010",
+        city: "san jacinto",
+        state: "ca",
+        country: "us",
+        shape: "unknown",
+        durationMinutes: "5 secondes",
+        comments: "A bright light hitting us while leaving at high speed"
+    },
+    {
+        datetime: "1/10/2010",
+        city: "norton",
+        state: "ma",
+        country: "us",
+        shape: "triangle",
+        durationMinutes: "1 min",
+        comments: "strange craft with amazing speed."
+    },
+    {
+        datetime: "1/10/2010",
+        city: "hancock",
+        state: "md",
+        country: "us",
+        shape: "sphere",
+        durationMinutes: "2 minutes",
+        comments: "Photo taken of silver colored sphere in Western Maryland."
+    },
+    {
+        datetime: "1/10/2010",
+        city: "cary",
+        state: "nc",
+        country: "us",
+        shape: "changing",
+        durationMinutes: "15 min",
+        comments: "Object appears and hovers&#44 strange maneuvering."
+    },
+    {
+        datetime: "1/10/2010",
+        city: "tularosa",
+        state: "nm",
+        country: "us",
+        shape: "other",
+        durationMinutes: "5 min.",
+        comments: "green figure 8 followed by military aircraft"
+    },
+    {
+        datetime: "1/10/2010",
+        city: "eunice",
+        state: "la",
+        country: "us",
+        shape: "oval",
+        durationMinutes: "15:00 mins",
+        comments: "Two objects meet then leave each other one to the east and one to the west"
+    },
+    {
+        datetime: "1/10/2010",
+        city: "butler",
+        state: "mo",
+        country: "us",
+        shape: "chevron",
+        durationMinutes: "1-2 minutes",
+        comments: "((HOAX??))  I was heading driving to the store when I seen a red and yellow lighted craft in the air flying 1000 ft or less strobing."
+    },
+    {
+        datetime: "1/10/2010",
+        city: "lompoc",
+        state: "ca",
+        country: "us",
+        shape: "fireball",
+        durationMinutes: "6 sec",
+        comments: "huge bright orange sphere appeared and dissapeared"
+    },
+    {
+        datetime: "1/10/2010",
+        city: "pratt",
+        state: "ks",
+        country: "us",
+        shape: "circle",
+        durationMinutes: "6 seconds",
+        comments: "4 rings of light accompanied by a single ring of light"
+    },
+    {
+        datetime: "1/11/2010",
+        city: "taunton",
+        state: "ma",
+        country: "us",
+        shape: "unknown",
+        durationMinutes: "10 seconds",
+        comments: "((HOAX??))  saw a strange object in the night sky"
+    },
+    {
+        datetime: "1/11/2010",
+        city: "lakeland",
+        state: "fl",
+        country: "us",
+        shape: "other",
+        durationMinutes: "1 minute",
+        comments: "Neon blue light in the sky disappears and then phone call is interrupted by unknown sound ."
+    },
+    {
+        datetime: "1/11/2010",
+        city: "fort myers",
+        state: "fl",
+        country: "us",
+        shape: "fireball",
+        durationMinutes: "15 seconds",
+        comments: "Bright fireball seen over Naples area of SW Florida."
+    },
+    {
+        datetime: "1/11/2010",
+        city: "fairfield",
+        state: "ca",
+        country: "us",
+        shape: "oval",
+        durationMinutes: "unknown",
+        comments: "Shape was black in color"
+    },
+    {
+        datetime: "1/11/2010",
+        city: "joplin",
+        state: "mo",
+        country: "us",
+        shape: "triangle",
+        durationMinutes: "5 to 10 sec.",
+        comments: "V shaped&#44 extreamly fast&#44 silent and the color was orange."
+    },
+    {
+        datetime: "1/11/2010",
+        city: "leatherwood",
+        state: "ky",
+        country: "us",
+        shape: "other",
+        durationMinutes: "5 minutes",
+        comments: "3 SQUARE-SHAPED OBJECTS WITH RED LIGHTS IN A TRIANGULAR FORM - KENTUCKY"
+    },
+    {
+        datetime: "1/11/2010",
+        city: "ingleside (canada)",
+        state: "on",
+        country: "ca",
+        shape: "triangle",
+        durationMinutes: "5 minutes",
+        comments: "triangle shaped craft with four coloured lights"
+    },
+    {
+        datetime: "1/12/2010",
+        city: "fayetteville",
+        state: "wv",
+        country: "us",
+        shape: "sphere",
+        durationMinutes: "7 minuts",
+        comments: "something followed my family and left a piece behind"
+    },
+    {
+        datetime: "1/12/2010",
+        city: "eugene",
+        state: "or",
+        country: "us",
+        shape: "unknown",
+        durationMinutes: "2 minutes",
+        comments: "Awaken by several episodes of BRIGHT FLASHING LIGHT and a very LOUD mysterous noise."
+    },
+    {
+        datetime: "1/12/2010",
+        city: "springfield",
+        state: "or",
+        country: "us",
+        shape: "changing",
+        durationMinutes: "3 minutes",
+        comments: "black floating object with lights"
+    },
+    {
+        datetime: "1/12/2010",
+        city: "alma",
+        state: "ar",
+        country: "us",
+        shape: "other",
+        durationMinutes: "night",
+        comments: "((HOAX??))  a strange ball of fire&#44 like after burners."
+    },
+    {
+        datetime: "1/12/2010",
+        city: "wichita",
+        state: "ks",
+        country: "us",
+        shape: "teardrop",
+        durationMinutes: "5 minutes",
+        comments: "Orange teardrop-shape craft"
+    },
+    {
+        datetime: "1/12/2010",
+        city: "deep run",
+        state: "nc",
+        country: "us",
+        shape: "other",
+        durationMinutes: "10 seconds",
+        comments: "Car like object in the sky"
+    },
+    {
+        datetime: "1/12/2010",
+        city: "waukesha",
+        state: "wi",
+        country: "us",
+        shape: "disk",
+        durationMinutes: "15minutes",
+        comments: "five glowning globes over waukesha&#44wi"
+    },
+    {
+        datetime: "1/12/2010",
+        city: "ceres",
+        state: "ca",
+        country: "us",
+        shape: "fireball",
+        durationMinutes: "30 seconds",
+        comments: "Fireballs seen from Ceres&#44 California"
+    },
+    {
+        datetime: "1/13/2010",
+        city: "white oak",
+        state: "pa",
+        country: "us",
+        shape: "triangle",
+        durationMinutes: "30 seconds",
+        comments: "Blinking lights on a rectanglar shaped object spotted in the sky in White Oak&#44 PA."
+    },
+    {
+        datetime: "1/13/2010",
+        city: "ackerman",
+        state: "ms",
+        country: "us",
+        shape: "light",
+        durationMinutes: "2 hours",
+        comments: "A bright light hovering in the southwestern sky.  ((NUFORC Note:  Possible sighting of Sirius.  PD))"
+    },
+    {
+        datetime: "1/13/2010",
+        city: "gulf breeze",
+        state: "fl",
+        country: "us",
+        shape: "unknown",
+        durationMinutes: "4 minutes",
+        comments: "Bright orange object&#44 rapid speed and direction changes .."
+    }
+    ];

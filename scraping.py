a6023956466310c01d32d2e4d9c4c8ddfffaa4da
Annette Donald Blackburn <109121304+annette-blackburn@users.noreply.github.com>
2022-10-09 09:52:33 -0600

    Add files via upload

--- scraping.py
+++ scraping.py
@@ -18,3 +18,62 @@ browser.is_element_present_by_css('div.list_text', wait_time=1)
 # Convert the browser html to a soup object and then quit the browser
 html = browser.html
 news_soup = soup(html, 'html.parser')
+
+# Visit the mars nasa news site
+url = 'https://redplanetscience.com/'
+browser.visit(url)
+
+# Optional delay for loading the page
+browser.is_element_present_by_css('div.list_text', wait_time=1)
+
+# Convert the browser html to a soup object and then quit the browser
+html = browser.html
+news_soup = soup(html, 'html.parser')
+
+slide_elem = news_soup.select_one('div.list_text')
+slide_elem.find('div', class_='content_title')
+
+# Use the parent element to find the first 'a' tag and save it as 'news_title'
+news_title = slide_elem.find('div', class_='content_title').get_text()
+news_title
+
+# Use the parent element to find the paragraph text
+news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
+news_p
+
+def mars_news(browser):
+
+   # Scrape Mars News
+   # Visit the mars nasa news site
+   url = 'https://redplanetscience.com/'
+   browser.visit(url)
+
+   # Optional delay for loading the page
+   browser.is_element_present_by_css('div.list_text', wait_time=1)
+
+   # Convert the browser html to a soup object and then quit the browser
+   html = browser.html
+   news_soup = soup(html, 'html.parser')
+
+   slide_elem = news_soup.select_one('div.list_text')
+   slide_elem.find('div', class_='content_title')
+
+    # Add try/except for error handling
+    try:
+        slide_elem = news_soup.select_one('div.list_text')
+        # Use the parent element to find the first 'a' tag and save it as 'news_title'
+        news_title = slide_elem.find('div', class_='content_title').get_text()
+        # Use the parent element to find the paragraph text
+        news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
+
+    except AttributeError:
+        return None, None
+
+   # Use the parent element to find the first <a> tag and save it as  `news_title`
+   news_title = slide_elem.find('div', class_='content_title').get_text()
+   news_title
+
+   # Use the parent element to find the paragraph text
+   news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
+   news_p
+    return news_title, news_p
